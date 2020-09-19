72de66f210e1ec7e174492a0f3b6e5e8b0b84e90
Joy <jooy@interia.pl>
2020-09-19 12:22:30 +0200

    host changed

--- app.py
+++ app.py
@@ -90,6 +90,9 @@ app.layout = html.Div([
 
 
 
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5000)
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+#
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
